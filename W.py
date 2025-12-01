4d54c4c6cf616f1eb5244e622b631c5a9d5e12ca
Siddharth Yadav <siddharthyadav92675@gmail.com>
2025-12-02 00:26:25 +0530

    Add files via upload

---
+++ W.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+for i in range(n):
+    for j in range(n + i):
+        if j < n - i - 1:
+            print(" ", end="")
+        else:
+            print("*", end="")
+    print()
+
+for i in range(n-1, -1, -1):
+    for j in range(n + i):
+        if j < n - i - 1:
+            print(" ", end="")
+        else:
+            print("*", end="")
+    print()
