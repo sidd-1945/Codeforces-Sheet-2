42f251def16290bae19d90e040a6c55fdd7477ad
Siddharth Yadav <siddharthyadav92675@gmail.com>
2025-12-02 00:26:37 +0530

    Add files via upload

---
+++ Y.py
@@ -0,0 +1,13 @@
+N = int(input())
+a = 0   
+b = 1   
+if N >= 1:
+    print(a, end=" ")
+if N >= 2:
+    print(b, end=" ")
+
+for i in range(3, N + 1):
+    c = a + b
+    print(c, end=" ")
+    a = b
+    b = c
